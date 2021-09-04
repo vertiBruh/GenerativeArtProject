57b0ef2ccb55e3bded1abe560e3fa38ad855e667
Ajmal Banoo <ajbanoo@gmail.com>
2021-09-04 21:14:21 +0200

    chore(project): remove unnecessary code

--- GenerativeArtProject/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace GenerativeArtProject.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- GenerativeArtProject/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace GenerativeArtProject.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
