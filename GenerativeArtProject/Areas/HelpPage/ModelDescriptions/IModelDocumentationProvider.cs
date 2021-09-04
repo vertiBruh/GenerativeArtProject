afcd7dd53af335a3ceb51a03ebb3e50bfe4375c1
Ajmal Banoo <ajbanoo@gmail.com>
2021-09-04 20:52:49 +0200

    feat(project): intial

---
+++ GenerativeArtProject/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace GenerativeArtProject.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
---
+++ GenerativeArtProject/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace GenerativeArtProject.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
